5932f70fa831ed62d11d1e414b4a94e177848f7f
brianromo <105336709+brianromo@users.noreply.github.com>
2022-09-01 12:27:18 -0400

    Add files via upload

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,13 +1,32 @@
+/************************************************************
+ * Brian Banfield
+ * 9/4/2022
+ * Fibonacci Sequence. 
+ ***********************************************************/
+
 package fibonacci;
 
-class FibonacciSequence {
+/**
+ * Demonstrates nth term of the Fibonacci sequence.
+ * @author Brian Banfield
+ *
+ */
 
+class FibonacciSequence {
+		/**
+		 * This is main class.
+		 * @param args input arguments not used.
+		 */
 	    public static void main(String[] args) {
 	        int n = 10;
 	        int y = Fibonacci(n);
 	        System.out.println("The " + n + "th term of the Fibonacci sequence is " + y + ".");
 	    }
-
+	    /**
+	     * This method returns the nth term of the Fibonacci sequence.
+	     * @param n The nth term of the Fibonacci sequence.
+	     * @return Fibonacci number for n. 
+	     */	    
 	    public static int Fibonacci(int n) {
 	        if(n == 0) {
 	            return 0;
